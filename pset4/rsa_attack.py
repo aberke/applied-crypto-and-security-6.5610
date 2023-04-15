124fc0bed79cf4b5abbcd4b5fea11d6992c303ec
Alex Berke <aberke@Alexs-MacBook-Pro.local>
2023-04-15 13:03:34 -0400

    completed code for pset4

--- pset4/rsa_attack.py
+++ pset4/rsa_attack.py
@@ -1,10 +1,12 @@
-# pip install rsa
+import gmpy2
 import rsa
 
 EXPONENT = 5
 KEYSIZE = 2048
 
+# https://stuvel.eu/python-rsa-doc/reference.html#rsa.newkeys
 def generate_keys():
+    """Returns a tuple (rsa.PublicKey, rsa.PrivateKey)"""
     return rsa.newkeys(KEYSIZE, exponent=EXPONENT)
 
 # Encrypt without padding
@@ -31,11 +33,32 @@ def broadcast_attack():
     cf = open("ciphertexts.txt")
     moduli = [int(k) for k in kf.readlines()]
     as_nums = [int(c) for c in cf.readlines()]
-    # TODO: your attack
-    r = 0
-
-
-    # Print out string for answer
-    print(str(rsa.transform.int2bytes(r)))
+    # -------------------------------------------------
+    # Skeleton code above; My(*) code below.
+    # (*) Help from https://bitsdeep.com/posts/attacking-rsa-for-fun-and-ctf-points-part-2/
+    # -------------------------------------------------
+    """
+    Call secret message m, note EXPONENT e=5
+    M = m^(EXPONENT) 
+    Need to solve system of equations:
+    M ≡ c1 (mod n1)
+    M ≡ c2 (mod n2)
+    M ≡ c3 (mod n3)
+    M ≡ c4 (mod n4)
+    M ≡ c5 (mod n5)
+    Assume the n_i (pk's) are coprime ==> chinese remainder thrm: unique solution exists
+    """
+    n1, n2, n3, n4, n5 = moduli
+    cs = as_nums
+    N = n1*n2*n3*n4*n5
+    # N_i = N/n_i: N1 = N/n1, N2 = N/n2, etc
+    Ns = [N//moduli[i] for i in range(5)]
+    inverses = [gmpy2.invert(Ns[i], moduli[i]) for i in range(5)]
+    M = sum([cs[i]*inverses[i]*Ns[i] for i in range (5)]) % N
+    m = gmpy2.iroot(M, EXPONENT)[0]
+    print('int version of m:')
+    print(m)
+    print('string:')
+    print(str(rsa.transform.int2bytes(int(m))))
 
 broadcast_attack()
